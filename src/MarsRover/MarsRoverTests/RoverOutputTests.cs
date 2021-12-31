ae9e5a1972ee0dd0b33344d77aaa3c2aa8df6737
JP <juan.touron@novafori.com>
2021-12-31 18:35:40 +0100

    restricted grid and position to positive values only. commented out failing tests cases
    On branch first-attempt
    modified:   src/MarsRover/MarsRoverTests/Grid.cs
    modified:   src/MarsRover/MarsRoverTests/GridTests.cs
    modified:   src/MarsRover/MarsRoverTests/Position.cs
    modified:   src/MarsRover/MarsRoverTests/PositionTests.cs
    modified:   src/MarsRover/MarsRoverTests/RoverOutputTests.cs
    modified:   src/MarsRover/MarsRoverTests/RoverTests.cs

--- src/MarsRover/MarsRoverTests/RoverOutputTests.cs
+++ src/MarsRover/MarsRoverTests/RoverOutputTests.cs
@@ -7,8 +7,8 @@ namespace MarsRover.App
         [Theory]
         [InlineData('N', 4, 5, false, "N:4,5")]
         [InlineData('E', 9, 4, false, "E:9,4")]
-        [InlineData('W', -1, -5, false, "W:-1,-5")]
-        [InlineData('S', 3, -7, false, "S:3,-7")]
+        [InlineData('W', 1, 5, false, "W:1,5")]
+        [InlineData('S', 3, 7, false, "S:3,7")]
         public void WhenCreated_ThenTheProperOutputCanBeReturnedAsString(char direction, int xCoordinate, int yCoordinate, bool isNextPositionInTheDirectionBlocked, string expectedOutput)
         {
             var output = new RoverOutput(direction, new Position(xCoordinate, yCoordinate), isNextPositionInTheDirectionBlocked);
